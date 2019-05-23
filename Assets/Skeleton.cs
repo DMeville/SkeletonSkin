d7aae8afca02bfc648d7139813074eb5eecb00df
Dylan Meville <dylan@dylanmeville.com>
2019-05-23 13:40:31 -0600

    No longer requires Odin
    Changed it to use lists instead of dictionaries (which would not serialize at edit time and get cleared on play causing no equipment to work). Works great now.
    
    Uses NaughtInspectorAttributes for a precache button, which is free. Comment that out if you don't wanna use it and just want to use the right click context menu to precache everything

--- Assets/Skeleton.cs
+++ Assets/Skeleton.cs
@@ -1,21 +1,28 @@
-﻿using System.Collections;
+﻿using NaughtyAttributes;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using Sirenix.OdinInspector;
+//using Sirenix;
+//using Sirenix.OdinInspector;
 
-public class Skeleton : SerializedMonoBehaviour {
-//public class Skeleton : MonoBehaviour { 
+public class Skeleton : MonoBehaviour {
 
-    [SerializeField]
-    public Dictionary<string, BoneDefintiion> rigDefinition = new Dictionary<string, BoneDefintiion>();
+    //public Dictionary<string, BoneDefintiion> rigDefinition = new Dictionary<string, BoneDefintiion>();
+    public List<BoneDefintion> rigDefinition = new List<BoneDefintion>();
     public Transform rootBone;
 
-    [Button(Name = "Precache Rig")]
+    //replace dictionary with List<BoneDefinition>.
+    //but we need to be able
+
+    //[Button(Name = "Precache Rig")]
+    //[ContextMenu("")]
+    [Button]
     public void PrecacheRig() {
-        rigDefinition = new Dictionary<string, BoneDefintiion>();
+        rigDefinition = new List<BoneDefintion>();
         Transform[] children = rootBone.GetComponentsInChildren<Transform>();
-        for(int i = 0;  i < children.Length; i++) {
-            rigDefinition.Add(children[i].name, new BoneDefintiion() { index = i, gameObject = children[i] });
+        for(int i = 0; i < children.Length; i++) {
+            BoneDefintion bd = new BoneDefintion() { index = i, name = children[i].name, gameObject = children[i] };
+            rigDefinition.Add(bd);
         }
     }
 
@@ -28,20 +35,47 @@ public class Skeleton : SerializedMonoBehaviour {
     //Skeletons must match!
     public void EquipSkin(SkeletonSkin skin) {
         List<Transform> b = new List<Transform>();
-        Debug.Log(skin.skeleton.Count);
         foreach(var bone in skin.skeleton) {
-            b.Add(rigDefinition[bone.Key].gameObject.transform);
+            //we need a list (b) that has all the bones that these two share?   
+            //for every bone precached in the skin
+            //we need to find that corresponding bone in our rigDef and add it to a list
+
+            //find bone with name bone (string value)
+            BoneDefintion bd = rigDefinition.Find((d) => bone == d.name);
+
+            b.Add(rigDefinition[bd.index].gameObject.transform);
         }
 
         skin.smr.bones = b.ToArray();
         skin.smr.rootBone = rootBone;
         skin.smr.transform.parent = rootBone.parent;
-        skin.smr.sharedMesh.RecalculateBounds();
+    }
+
+    //If we have a shirt that's already on one player, we can't just use the Equip method because it places it in the wrong spot?
+    //Use this instead I guess
+    public void ReassignSkin(SkeletonSkin equip, Skeleton skel) {
+        List<Transform> b = new List<Transform>();
+        foreach(var e in equip.skeleton) {
+            BoneDefintion bd = skel.rigDefinition.Find((d) => e == d.name);
+            b.Add(skel.rigDefinition[bd.index].gameObject.transform);
+        }
+
+        SkinnedMeshRenderer s = equip.GetComponent<SkinnedMeshRenderer>();
+        s.bones = b.ToArray();
+        s.rootBone = skel.rootBone;
+        s.transform.parent = skel.rootBone.parent.parent;
+
+    }
+
+    [ContextMenu("LogRigDef")]
+    public void LogRigDef() {
+        Debug.Log("Skeleton Rigdef: " + rigDefinition.Count);
     }
 }
 
 [System.Serializable]
-public class BoneDefintiion {
+public class BoneDefintion {
     public int index;
+    public string name;
     public Transform gameObject;
 }
--- Assets/SkeletonSkin.cs
+++ Assets/SkeletonSkin.cs
@@ -1,28 +1,36 @@
-﻿using System.Collections;
+﻿using NaughtyAttributes;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using Sirenix.OdinInspector;
+//using Sirenix.OdinInspector;
 
-public class SkeletonSkin : SerializedMonoBehaviour {
-//public class SkeletonSkin : MonoBehaviour {
+public class SkeletonSkin : MonoBehaviour {
 
     [SerializeField]
-    public Dictionary<string, int> skeleton = new Dictionary<string, int>();
+    //public Dictionary<string, int> skeleton = new Dictionary<string, int>();
+    public List<string> skeleton = new List<string>();
     public SkinnedMeshRenderer smr;
 
-    [Button(Name = "Precache Bones")]
-    public void PrecacheBones() {
-        skeleton = new Dictionary<string, int>();
-        smr = this.GetComponent<SkinnedMeshRenderer>();
-        Transform[] ts = smr.bones;
-        for(int i = 0; i < ts.Length; i++) {
-            skeleton.Add(ts[i].name, i);
-        }
-    }
 
+    //[Button(Name = "Precache Bones")]
+    [Button]
+    public void PrecacheBones() {
+        skeleton = new List<string>();
+        Transform[] ts = smr.GetComponent<SkinnedMeshRenderer>().bones;
+        for(int i = 0; i < ts.Length; i++) {
+            skeleton.Add(ts[i].name);
+        }
+
+        smr = this.GetComponent<SkinnedMeshRenderer>();
+    }
 
     [ContextMenu("PrecacheBones")]
     public void PrecacheBonesCM() {
         PrecacheBones();
+    }
+
+    [ContextMenu("LogRigDef")]
+    public void LogRigDef() {
+        Debug.Log("SkeletonSkin skeleton: " + skeleton.Count);
     }
 }
